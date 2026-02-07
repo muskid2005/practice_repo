168c57bab6a09de4a974ed96c7518dd9d8e7731f
Mustapha <mustaphabusari2005@gmail.com>
2026-02-07 12:30:35 +0100

    updated the app.js code

--- app.js
+++ app.js
@@ -7,6 +7,22 @@ app.get("/", (req, res) => {
   res.sendFile("home.html", { root: "." });
 });
 
+app.get("/about", (req, res) => {
+  res.send("THIS IS THE ABOUT PAGE");
+});
+
+app.get("/pricing", (req, res) => {
+  res.send("THIS IS THE PRICING PAGE");
+});
+
+app.get("/contact", (req, res) => {
+  res.status(200).json({
+    Message: "THIS IS THE CONTACT PAGE",
+    Name: "MUSTAPHA",
+    Email: "mustaphabusari2005@gmail.com",
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
 });
